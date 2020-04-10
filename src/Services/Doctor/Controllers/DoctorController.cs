3d898032c74a7645dca44f14ae197ce255783c07
Arnab Roy Chowdhury <arnab-roy-chowdhury@outlook.com>
2020-04-10 12:34:08 +0530

    Create crud for doctor service.

---
+++ src/Services/Doctor/Controllers/DoctorController.cs
@@ -0,0 +1,63 @@
+﻿using MedicalSystem.Services.Doctor.Data;
+using MedicalSystem.Services.Doctor.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MedicalSystem.Services.Doctor.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DoctorController
+    {
+        private readonly DoctorContext _doctorContext;
+
+        public DoctorController(DoctorContext doctorContext)
+        {
+            _doctorContext = doctorContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<DoctorModel> GetAllDoctors()
+        {
+            var doctors = _doctorContext.Doctors.OrderBy(doctor => doctor.FirstName);
+            return doctors;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public DoctorModel GetById(int id)
+        {
+            var doctor = _doctorContext.Doctors.FirstOrDefault(doctor => doctor.Id == id);
+            return doctor;
+        }
+
+        [HttpPost]
+        public void Add(DoctorModel doctor)
+        {
+            _doctorContext.Doctors.Add(doctor);
+            _doctorContext.SaveChanges();
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public void Update(int id, DoctorModel doctor)
+        {
+            var d = _doctorContext.Doctors.FirstOrDefault(doctor => doctor.Id == id);
+
+            d.FirstName = doctor.FirstName;
+            d.LastName = doctor.LastName;
+
+            _doctorContext.SaveChanges();
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public void Delete(int id)
+        {
+            var doctor = _doctorContext.Doctors.FirstOrDefault(doctor => doctor.Id == id);
+            _doctorContext.Doctors.Remove(doctor);
+            _doctorContext.SaveChanges();
+        }
+    }
+}
---
+++ src/Services/Doctor/Data/DoctorContext.cs
@@ -0,0 +1,15 @@
+﻿using MedicalSystem.Services.Doctor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalSystem.Services.Doctor.Data
+{
+    public class DoctorContext : DbContext
+    {
+        public DoctorContext(DbContextOptions<DoctorContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DoctorModel> Doctors { get; set; }
+    }
+}
