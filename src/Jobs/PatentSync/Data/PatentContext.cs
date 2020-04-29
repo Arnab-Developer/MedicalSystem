c720bfb0abe745d2bd9f1e15255f36940db91cbb
Arnab Roy Chowdhury <arnab-roy-chowdhury@outlook.com>
2020-04-29 12:56:46 +0530

    Add patent sync job.

---
+++ src/Jobs/PatentSync/Data/PatentContext.cs
@@ -0,0 +1,22 @@
+﻿using MedicalSystem.Jobs.PatentSync.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalSystem.Jobs.PatentSync.Data
+{
+    internal class PatentContext : DbContext
+    {
+        /// <summary>
+        /// Creates a new DbContext object.
+        /// </summary>
+        /// <param name="options"></param>
+        public PatentContext(DbContextOptions<PatentContext> options)
+            : base(options)
+        {
+        }
+
+        /// <summary>
+        /// DbSet for patent.
+        /// </summary>
+        public DbSet<PatentModel>? Patents { get; set; }
+    }
+}
---
+++ src/Jobs/PatentSync/Models/PatentModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MedicalSystem.Jobs.PatentSync.Models
+{
+    internal class PatentModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public PatentModel()
+        {
+            Id = 0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+        }
+    }
+}
