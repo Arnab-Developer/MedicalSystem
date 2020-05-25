c12b9eee119a7a42406de441e7754e0de2a0b819
Arnab Roy Chowdhury <arnab-roy-chowdhury@outlook.com>
2020-05-25 13:44:54 +0530

    Xml comments for gateway options. Issue #18.

--- src/Gateways/WebGateway/Options/ConsultationOptions.cs
+++ src/Gateways/WebGateway/Options/ConsultationOptions.cs
@@ -1,11 +1,18 @@
 ﻿namespace MedicalSystem.Gateways.WebGateway.Options
 {
+    /// <include file='docs.xml' path='docs/members[@name="ConsultationOptions"]/consultationOptions/*'/>
     public class ConsultationOptions
     {
+        /// <include file='docs.xml' path='docs/members[@name="ConsultationOptions"]/consultationApiUrl/*'/>
         public string ConsultationApiUrl { get; set; }
+
+        /// <include file='docs.xml' path='docs/members[@name="ConsultationOptions"]/consultationDoctorApiUrl/*'/>
         public string ConsultationDoctorApiUrl { get; set; }
+
+        /// <include file='docs.xml' path='docs/members[@name="ConsultationOptions"]/consultationPatentApiUrl/*'/>
         public string ConsultationPatentApiUrl { get; set; }
 
+        /// <include file='docs.xml' path='docs/members[@name="ConsultationOptions"]/consultationOptionsConstructor/*'/>
         public ConsultationOptions()
         {
             ConsultationApiUrl = string.Empty;
--- src/Gateways/WebGateway/Options/DoctorOptions.cs
+++ src/Gateways/WebGateway/Options/DoctorOptions.cs
@@ -1,9 +1,12 @@
 ﻿namespace MedicalSystem.Gateways.WebGateway.Options
 {
+    /// <include file='docs.xml' path='docs/members[@name="DoctorOptions"]/doctorOptions/*'/>
     public class DoctorOptions
     {
+        /// <include file='docs.xml' path='docs/members[@name="DoctorOptions"]/doctorApiUrl/*'/>
         public string DoctorApiUrl { get; set; }
 
+        /// <include file='docs.xml' path='docs/members[@name="DoctorOptions"]/doctorOptionsConstructor/*'/>
         public DoctorOptions()
         {
             DoctorApiUrl = string.Empty;
