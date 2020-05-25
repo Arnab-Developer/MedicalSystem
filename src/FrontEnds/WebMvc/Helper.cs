658da7897e9458922a80a4487f992d615955ba60
Arnab Roy Chowdhury <arnab-roy-chowdhury@outlook.com>
2020-05-25 21:06:10 +0530

    Show web mvc application version number in UI.

---
+++ src/FrontEnds/WebMvc/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+namespace MedicalSystem.FrontEnds.WebMvc
+{
+    public static class Helper
+    {
+        static Helper()
+        {
+            ApplicationVersionNumber = string.Empty;
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            if (executingAssembly == null) return;
+            var assemblyName = executingAssembly.GetName();
+            if (assemblyName == null) return;
+            var assemblyVersion = assemblyName.Version;
+            if (assemblyVersion == null) return;
+            ApplicationVersionNumber = $" - Version {assemblyVersion.ToString(2)}";
+        }
+
+        public static string ApplicationVersionNumber { get; }
+    }
+}
