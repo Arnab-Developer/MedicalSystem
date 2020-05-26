180245d7ceb85cb71003b7c9d23eb96cdf0b2bc8
Arnab Roy Chowdhury <arnab-roy-chowdhury@outlook.com>
2020-05-26 11:01:18 +0530

    Xml comments.

--- src/FrontEnds/WebMvc/Options/DoctorOptions.cs
+++ src/FrontEnds/WebMvc/Options/DoctorOptions.cs
@@ -1,9 +1,12 @@
 ﻿namespace MedicalSystem.FrontEnds.WebMvc.Options
 {
+    /// <include file='docs.xml' path='docs/members[@name="DoctorOptions"]/doctorOptions/*'/>
     public class DoctorOptions
     {
+        /// <include file='docs.xml' path='docs/members[@name="DoctorOptions"]/doctorGatewayUrl/*'/>
         public string DoctorGatewayUrl { get; set; }
 
+        /// <include file='docs.xml' path='docs/members[@name="DoctorOptions"]/doctorOptionsConstructor/*'/>
         public DoctorOptions()
         {
             DoctorGatewayUrl = string.Empty;
--- src/FrontEnds/WebMvc/Options/PatentOptions.cs
+++ src/FrontEnds/WebMvc/Options/PatentOptions.cs
@@ -1,9 +1,12 @@
 ﻿namespace MedicalSystem.FrontEnds.WebMvc.Options
 {
+    /// <include file='docs.xml' path='docs/members[@name="PatentOptions"]/patentOptions/*'/>
     public class PatentOptions
     {
+        /// <include file='docs.xml' path='docs/members[@name="PatentOptions"]/patentGatewayUrl/*'/>
         public string PatentGatewayUrl { get; set; }
 
+        /// <include file='docs.xml' path='docs/members[@name="PatentOptions"]/patentOptionsConstructor/*'/>
         public PatentOptions()
         {
             PatentGatewayUrl = string.Empty;
