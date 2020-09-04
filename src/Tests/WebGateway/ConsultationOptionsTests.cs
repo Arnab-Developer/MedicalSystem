a1d3f14640bd233c9b3702eebbaa6dfe74b32318
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2020-09-04 14:07:53 +0530

    Consultation gateway options tests (#44)

---
+++ src/Tests/WebGateway/ConsultationOptionsTests.cs
@@ -0,0 +1,20 @@
+﻿using MedicalSystem.Gateways.WebGateway.Options;
+using NUnit.Framework;
+
+namespace MedicalSystem.Tests.Gateways.WebGateway
+{
+    /// <include file='docs.xml' path='docs/members[@name="ConsultationModelTests"]/consultationModelTests/*'/>
+    internal class ConsultationOptionsTests
+    {
+        /// <include file='docs.xml' path='docs/members[@name="ConsultationModelTests"]/consultationModel_GivenValid_ReturnsValid/*'/>
+        [Test]
+        public void ConsultationModel_GivenValid_ReturnsValid()
+        {
+            var consultationOptions = new ConsultationOptions
+            {
+                ConsultationApiUrl = "http://localhost:1234"
+            };
+            Assert.AreEqual("http://localhost:1234", consultationOptions.ConsultationApiUrl);
+        }
+    }
+}
