08f93a3a152642305296057721c091d065f12927
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2020-09-04 11:46:37 +0530

    Doctor gateway option test (#40)

---
+++ src/Tests/WebGateway/DoctorOptionsTests.cs
@@ -0,0 +1,20 @@
+﻿using MedicalSystem.Gateways.WebGateway.Options;
+using NUnit.Framework;
+
+namespace MedicalSystem.Tests.Gateways.WebGateway
+{
+    /// <include file='docs.xml' path='docs/members[@name="DoctorOptionsTests"]/doctorOptionsTests/*'/>
+    internal class DoctorOptionsTests
+    {
+        /// <include file='docs.xml' path='docs/members[@name="DoctorOptionsTests"]/doctorOptions_GivenValid_ReturnsValid/*'/>
+        [Test]
+        public void DoctorOptions_GivenValid_ReturnsValid()
+        {
+            var doctorOptions = new DoctorOptions
+            {
+                DoctorApiUrl = "http://localhost:1234"
+            };
+            Assert.AreEqual("http://localhost:1234", doctorOptions.DoctorApiUrl);
+        }
+    }
+}
