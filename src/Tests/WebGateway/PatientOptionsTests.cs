a6f18b412daec8b95b2465dc281d565329d60cf4
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2020-09-04 13:57:39 +0530

    Patient gateway options tests (#42)

---
+++ src/Tests/WebGateway/PatientOptionsTests.cs
@@ -0,0 +1,20 @@
+﻿using MedicalSystem.Gateways.WebGateway.Options;
+using NUnit.Framework;
+
+namespace MedicalSystem.Tests.Gateways.WebGateway
+{
+    /// <include file='docs.xml' path='docs/members[@name="PatientOptionsTests"]/patientOptionsTests/*'/>
+    internal class PatientOptionsTests
+    {
+        /// <include file='docs.xml' path='docs/members[@name="PatientOptionsTests"]/patientOptions_GivenValid_ReturnsValid/*'/>
+        [Test]
+        public void PatientOptions_GivenValid_ReturnsValid()
+        {
+            var patientOptions = new PatientOptions
+            {
+                PatientApiUrl = "http://localhost:1234"
+            };
+            Assert.AreEqual("http://localhost:1234", patientOptions.PatientApiUrl);
+        }
+    }
+}
