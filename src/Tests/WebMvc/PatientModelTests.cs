f41cdb6bf1fdb624a89ae0a9177fd849382c8452
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2020-09-09 10:22:28 +0530

    Patient model tests.

---
+++ src/Tests/WebMvc/PatientModelTests.cs
@@ -0,0 +1,22 @@
+﻿using MedicalSystem.FrontEnds.WebMvc.Models;
+using NUnit.Framework;
+
+namespace MedicalSystem.Tests.FrontEnds.WebMvc
+{
+    internal class PatientModelTests
+    {
+        [Test]
+        public void PatientModel_GivenValid_ReturnsValid()
+        {
+            var patientModel = new PatientModel
+            {
+                Id = 1,
+                FirstName = "f1",
+                LastName = "l1"
+            };
+            Assert.AreEqual(1, patientModel.Id);
+            Assert.AreEqual("f1", patientModel.FirstName);
+            Assert.AreEqual("l1", patientModel.LastName);
+        }
+    }
+}
