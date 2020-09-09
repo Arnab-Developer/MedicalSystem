04189e43e4e487738502bd18e7294e69180cbd01
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2020-09-09 10:30:07 +0530

    Doctor option tests.

---
+++ src/Tests/WebMvc/DoctorOptionsTests.cs
@@ -0,0 +1,20 @@
+﻿using MedicalSystem.FrontEnds.WebMvc.Options;
+using NUnit.Framework;
+
+namespace MedicalSystem.Tests.FrontEnds.WebMvc
+{
+    /// <include file='docs.xml' path='docs/members[@name="DoctorOptionsTests"]/doctorOptionsTests/*'/>
+    internal class DoctorOptionsTests
+    {
+        /// <include file='docs.xml' path='docs/members[@name="DoctorOptionsTests"]/doctorOptions_GivenValid_ReturnsValid/*'/>
+        [Test]
+        public void DoctorOptions_GivenValid_ReturnsValid()
+        {
+            var doctorOptions = new DoctorOptions
+            {
+                DoctorGatewayUrl = "http://localhost:1234"
+            };
+            Assert.AreEqual("http://localhost:1234", doctorOptions.DoctorGatewayUrl);
+        }
+    }
+}
