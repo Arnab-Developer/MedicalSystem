4abc830f1e9a24dd25293a28d3ed734fe067ae12
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2020-09-09 10:22:12 +0530

    Doctor model test.

---
+++ src/Tests/WebMvc/DoctorModelTests.cs
@@ -0,0 +1,22 @@
+﻿using MedicalSystem.FrontEnds.WebMvc.Models;
+using NUnit.Framework;
+
+namespace MedicalSystem.Tests.FrontEnds.WebMvc
+{
+    internal class DoctorModelTests
+    {
+        [Test]
+        public void DoctorModel_GivenValid_ReturnsValid()
+        {
+            var doctorModel = new DoctorModel
+            {
+                Id = 1,
+                FirstName = "f1",
+                LastName = "l1"
+            };
+            Assert.AreEqual(1, doctorModel.Id);
+            Assert.AreEqual("f1", doctorModel.FirstName);
+            Assert.AreEqual("l1", doctorModel.LastName);
+        }
+    }
+}
